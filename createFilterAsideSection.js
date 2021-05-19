66bbce02a1c7802162532af8f69763d3bc6960d6
Tijani Fulani <Fulani07@gmail.com>
2021-05-19 18:38:14 +0100

    form label halfdone

--- createFilterAsideSection.js
+++ createFilterAsideSection.js
@@ -1,7 +1,11 @@
+import { createFilterSection } from './createFilterSection.js';
+
 function createFilterAsideSection() {
   const asideEl = document.createElement('aside');
   asideEl.setAttribute('class', 'filters-section');
 
+  const functionEl = createFilterSection();
+  asideEl.append(functionEl);
   return asideEl;
 }
 
